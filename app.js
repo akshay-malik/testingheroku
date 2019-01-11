85e2cd98572a89f00734fd01150204fb99fb38b0
akshay-malik <ak.shay.penn@gmail.com>
2019-01-11 11:59:10 -0500

    testing

--- app.js
+++ app.js
@@ -6,20 +6,3 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
-
-var neo4j = require('neo4j-driver').v1;
-var driver = neo4j.driver("bolt://hobby-geefdaeefcom.dbs.graphenedb.com:24786", neo4j.auth.basic("v303", "GtGq5rldxu"));
-
-var session = driver.session();
-session
-    .run("CREATE (n:Person {name:'Bob'}) RETURN n.name")
-    .then(function(result) {
-        result.records.forEach(function(record) {
-            console.log(record)
-        });
-
-        session.close();
-    })
-    .catch(function(error) {
-        console.log(error);
-    });
